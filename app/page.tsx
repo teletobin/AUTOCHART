e5f7a3f76800db148ece002c14994c59b7e5da37
eunbin <eunbinga@naver.com>
2026-09-11 00:58:33 +0900

    테이블 헤더: 미사용, 삭제 열 이름 추가

--- app/page.tsx
+++ app/page.tsx
@@ -371,7 +371,8 @@ export default function Home() {
                     <span style={{ width: 58, textAlign: "center" }}>단가</span>
                     <span style={{ width: 40, textAlign: "center" }}>수량</span>
                     <span style={{ width: 76, textAlign: "center" }}>합계</span>
-                    <span style={{ width: 40 }} />
+                    <span style={{ width: 24, textAlign: "center" }}>미사용</span>
+                    <span style={{ width: 16, textAlign: "center" }}>삭제</span>
                   </div>
                   {selectedItems.map((item) => (
                     <div key={item.id} style={styles.tableRow}>
